1d53d65c04df766ba9074adb6359470256178bd1
jinwoongBang <dlopo123@gmail.com>
2018-11-05 15:07:27 +0900

    [2018_11_05_방진웅] 1) 보드 및 멤버 테이블 DB 생성 SQL 문 수정본

--- LaonZena/프로젝트_20181026/laonzena/src/main/resources/sql/보드테이블 및 멤버 테이블_181105.sql
+++ LaonZena/프로젝트_20181026/laonzena/src/main/resources/sql/보드테이블 및 멤버 테이블_181105.sql
@@ -1,3 +1,5 @@
+-- member --
+
 CREATE TABLE lz_tb_member (
     member_no number,
     member_id varchar2(50)  not null unique,
@@ -40,8 +42,11 @@ select *
 from lz_tb_member;
 
 -- board --
+drop table lz_tb_se_board;
 
-create table lz_tb_se_board(
+create sequence lz_s_gs_board_no increment by 1;
+
+create table lz_tb_gs_board(
     board_no number not null PRIMARY KEY,
     board_title varchar2(200) not null,
     board_reg_date date DEFAULT sysdate not null ,
@@ -49,5 +54,19 @@ create table lz_tb_se_board(
     board_product varchar2(200) null,
     board_view_cnt number not null,
     board_recom_cnt number not null,
+    attach_ori_name varchar2(255),
+    attach_ser_name varchar2(255),
+	attach_ser_path varchar2(255),
     board_writer REFERENCES lz_tb_member(member_id)  
 );
+
+-- comment
+create table lz_tb_se_comment(
+    comment_no number primary key,
+    board_no REFERENCES lz_tb_se_board(board_no),
+    comment_content varchar2(50),
+    comment_writer REFERENCES lz_tb_member(member_id)
+);
+
+create sequence lz_s_gs_comment_no increment by 1;
+
